5cb82acbea03c56b63f676926e44219c872e1169
drorpa <59962197+drorpa@users.noreply.github.com>
2021-03-22 16:49:56 +0200

    persist data

--- service1/src/main.py
+++ service1/src/main.py
@@ -9,6 +9,13 @@ class HelloWorldResource:
                           ' Gunicorn running in an Alpine Linux container.' +
                           ' this is SERVICE1')
 
+        p = request.params.get('write')
+        if p is None:
+            p = 'no input'
+
+        with open('container_data/myfile.txt', 'a') as txtfile:
+            txtfile.write(p + '\n')
+
 
 app = falcon.API()
 app.add_route('/', HelloWorldResource())
